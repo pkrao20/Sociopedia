23b27c646b393e6581d8458069466f8781901e86
Prashant rao <95528457+pkrao20@users.noreply.github.com>
2024-01-08 01:55:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
+import 'dotenv/config';
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -21,7 +21,6 @@ import { users, posts } from "./data/index.js";
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
