c0d46fc774f8bb362593ced0de5d41d75a37b648
Prashant rao <95528457+pkrao20@users.noreply.github.com>
2024-01-21 04:42:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
@@ -29,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
+app.use(cors());
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 
