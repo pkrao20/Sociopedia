5926dfa4d9b8bd760cd70843708b2039327065a1
Prashant rao <ce200004039@iiti.ac.in>
2024-03-03 01:52:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import 'dotenv/config';
 import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -15,18 +14,13 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 
-/* CONFIGURATIONS */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
-// app.use(helmet());
-// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
@@ -34,42 +28,27 @@ app.use(cors());
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 
-// app.use(
-//   cors({
-//     origin: "https://sociopedia123.vercel.app",
-//     credentials: true,
-//   })
-// );
-
 app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://sociopedia123.vercel.app"
-    );
-    res.header(
-        "Access-Control-Allow-Origin",
-        "Origin,X-Requested-With,Content-Type,Accept",
-        "Access-Control-Allow-Methods: GET, DELETE, PUT, PATCH, HEAD, OPTIONS, POST"
-    );
-    next();
-    });
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://sociopedia123.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Origin",
+    "Origin,X-Requested-With,Content-Type,Accept",
+    "Access-Control-Allow-Methods: GET, DELETE, PUT, PATCH, HEAD, OPTIONS, POST"
+  );
+  next();
+});
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
 
-/* FILE STORAGE */
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
